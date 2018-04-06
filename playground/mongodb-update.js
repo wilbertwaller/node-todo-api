848a5db79aa04c416c1fdb496974e796a7b21d57
Wilbert Waller <wilbert.waller@gmail.com>
2018-04-05 19:18:14 -0600

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5ac5a3de3162b63c16f5a0ea')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    name: 'Wilbert'
+  }, {
+    $set: {
+      name: 'Jessica'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+});
